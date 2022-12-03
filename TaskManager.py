a5cbc4d2729cb95a2779bea48f758b7f987dc94d
Samuel Chiscari <79214849+ChiscariSamuelA3@users.noreply.github.com>
2022-12-03 13:09:35 +0200

    added stats for cpu and memory usage

--- TaskManager.py
+++ TaskManager.py
@@ -6,15 +6,34 @@ class TaskManager:
     def __init__(self):
         pass
 
-    def get_cpu(self):
-        return psutil.cpu_percent(interval=1)
+    def get_cpu_count(self):
+        # get total number of phiysical cores
+        # get total number of logical cores
+        return psutil.cpu_count(logical=False), psutil.cpu_count(logical=True)
 
-    def get_memory(self):
+    def get_cpu_freq(self):
+        # get cpu frequency
+        return psutil.cpu_freq()
+
+    def get_cpu_per_core(self):
+        # get cpu usage per physical core
+        return psutil.cpu_percent(interval=0.5, percpu=True)
+
+    def get_cpu_usage(self):
+        # get cpu usage per physical core
+        return psutil.cpu_percent(interval=0.5)
+
+    def get_memory_usage(self):
         # get used memory
         return psutil.virtual_memory().used
 
     def get_total_memory(self):
+        # get total memory
         return psutil.virtual_memory().total
+
+    def get_memory_stats(self):
+        # get memory stats
+        return psutil.virtual_memory()
 
     def get_partitions_info(self):
         # partitions usage
@@ -26,9 +45,9 @@ class TaskManager:
 
             # mount info in GB
             mount_info = psutil.disk_usage(partition.mountpoint)
-            mount_info_total = mount_info.total / 1024 / 1024 / 1024
-            mount_info_used = mount_info.used / 1024 / 1024 / 1024
-            mount_info_free = mount_info.free / 1024 / 1024 / 1024
+            mount_info_total = mount_info.total / (1024 ** 3)
+            mount_info_used = mount_info.used / (1024 ** 3)
+            mount_info_free = mount_info.free / (1024 ** 3)
             mount_info_percent = mount_info.percent
 
             partitions_info.append(
@@ -36,7 +55,8 @@ class TaskManager:
 
         return partitions_info
 
-    def get_network(self):
+    def get_network_usage(self):
+        # get network usage
         # converted to kb/s
 
         network_sent_1, network_received_1 = psutil.net_io_counters().bytes_sent / 1024, psutil.net_io_counters().bytes_recv / 1024
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PyQt5.QtChart import QChart, QPieSeries, QPieSlice
 import threading
 from TaskManager import TaskManager
-from PyQt5 import QtWidgets, QtChart
+from PyQt5 import QtWidgets, QtChart, QtCore
 import pyqtgraph as pg
 
 
@@ -16,9 +16,55 @@ def init_tab_content(x_range, y_range, x_label, y_label):
     return resource_plot
 
 
-def init_tab_layout(tab: QtWidgets.QWidget, resource_plot):
+def add_cpu_stats(tab, task_manager):
+    cpu_physical_cores, cpu_logical_cores = task_manager.get_cpu_count()
+    physical_cores_label = QtWidgets.QLabel("Physical cores: " + str(cpu_physical_cores))
+    logical_cores_label = QtWidgets.QLabel("Logical cores: " + str(cpu_logical_cores))
+    tab.layout.addWidget(physical_cores_label)
+    tab.layout.addWidget(logical_cores_label)
+
+    cpu_frequency = task_manager.get_cpu_freq()
+    cpu_freq_max_label = QtWidgets.QLabel("CPU max frequency: " + str(cpu_frequency[0]) + " MHz")
+    cpu_freq_min_label = QtWidgets.QLabel("CPU min frequency: " + str(cpu_frequency[1]) + " MHz")
+    cpu_freq_current_label = QtWidgets.QLabel("CPU current frequency: " + str(cpu_frequency[2]) + " MHz")
+    tab.layout.addWidget(cpu_freq_max_label)
+    tab.layout.addWidget(cpu_freq_min_label)
+    tab.layout.addWidget(cpu_freq_current_label)
+
+    cpu_core_string = ""
+    for index, cpu_per_core in enumerate(task_manager.get_cpu_per_core()):
+        if index % 3 == 0:
+            cpu_core_string += "\n"
+        cpu_core_string += "Core " + str(index) + ": " + str(cpu_per_core) + "%\t"
+
+    cpu_core_label = QtWidgets.QLabel("CPU cores usage: " + cpu_core_string)
+    tab.layout.addWidget(cpu_core_label, alignment=QtCore.Qt.AlignCenter)
+
+
+def add_memory_stats(tab, task_manager):
+    # get memory info in gb
+    memory_stats = task_manager.get_memory_stats()
+    # with 2 decimal places
+    memory_stats = [round(x / (1024 ** 3), 2) for x in memory_stats]
+    memory_total_label = QtWidgets.QLabel("Total memory: " + str(memory_stats[0]) + " GB")
+    memory_available_label = QtWidgets.QLabel("Available memory: " + str(memory_stats[1]) + " GB")
+    memory_free_label = QtWidgets.QLabel("In use memory: " + str(memory_stats[3]) + " GB")
+    memory_percent_label = QtWidgets.QLabel("Memory usage: " + str(memory_stats[4]) + "%")
+    tab.layout.addWidget(memory_total_label, alignment=QtCore.Qt.AlignCenter)
+    tab.layout.addWidget(memory_available_label, alignment=QtCore.Qt.AlignCenter)
+    tab.layout.addWidget(memory_free_label, alignment=QtCore.Qt.AlignCenter)
+    tab.layout.addWidget(memory_percent_label, alignment=QtCore.Qt.AlignCenter)
+
+
+def init_tab_layout(tab: QtWidgets.QWidget, resource_plot, tab_type, task_manager):
     tab.layout = QtWidgets.QVBoxLayout()
     tab.layout.addWidget(resource_plot)
+
+    if tab_type == 0:  # cpu
+        add_cpu_stats(tab, task_manager)
+    elif tab_type == 1:  # memory
+        add_memory_stats(tab, task_manager)
+
     tab.setLayout(tab.layout)
 
 
@@ -63,27 +109,48 @@ def update_monitoring_info(resource_usage, resource_curve, resource_value):
     resource_curve.setData(resource_usage)
 
 
-def update_plots(cpu_usage, memory_usage, disk_plot, network_sent_usage, network_received_usage, task_manager,
+def update_plots(cpu_tab, memory_tab, partitions_tab, network_tab, cpu_usage, memory_usage, disk_plot,
+                 network_sent_usage, network_received_usage, task_manager,
                  cpu_curve,
                  memory_curve, network_sent_curve, network_received_curve, app):
     while True:
         try:
+            ###########################################################################################
             # update cpu usage
-            update_monitoring_info(cpu_usage, cpu_curve, task_manager.get_cpu())
+            update_monitoring_info(cpu_usage, cpu_curve, task_manager.get_cpu_usage())
+            # get cpu per core
+            cpu_core_string = ""
+            for index, cpu_per_core in enumerate(task_manager.get_cpu_per_core()):
+                if index % 3 == 0:
+                    cpu_core_string += "\n"
+                cpu_core_string += "Core " + str(index) + ": " + str(cpu_per_core) + "%\t"
+            # update label
+            cpu_tab.layout.itemAt(6).widget().setText("CPU cores usage: " + cpu_core_string)
 
+            ############################################################################################
             # update memory usage
-            update_monitoring_info(memory_usage, memory_curve, task_manager.get_memory())
+            update_monitoring_info(memory_usage, memory_curve, task_manager.get_memory_usage())
+            # update memory stats
+            memory_stats = task_manager.get_memory_stats()
+            memory_stats = [round(x / (1024 ** 3), 2) for x in memory_stats]
+            memory_tab.layout.itemAt(1).widget().setText("Total memory: " + str(memory_stats[0]) + " GB")
+            memory_tab.layout.itemAt(2).widget().setText("Available memory: " + str(memory_stats[1]) + " GB")
+            memory_tab.layout.itemAt(3).widget().setText("In use memory: " + str(memory_stats[3]) + " GB")
+            memory_tab.layout.itemAt(4).widget().setText("Memory usage: " + str(memory_stats[4]) + "%")
 
+            ############################################################################################
             # update partitions usage
             partitions_info = task_manager.get_partitions_info()
             disk_plot.update()
             update_partitions_info(disk_plot, partitions_info)
 
+            ############################################################################################
             # update network usage
-            network_info = task_manager.get_network()
+            network_info = task_manager.get_network_usage()
             update_monitoring_info(network_sent_usage, network_sent_curve, network_info[0])
             update_monitoring_info(network_received_usage, network_received_curve, network_info[1])
 
+            ############################################################################################
             app.processEvents()
 
         except Exception as e:
@@ -106,6 +173,7 @@ def main():
     # add tabs for each resource
     tabs = QtWidgets.QTabWidget()
 
+    # add a tab for cpu with 2 layouts
     cpu_tab = QtWidgets.QWidget()
     tabs.addTab(cpu_tab, "CPU")
 
@@ -137,16 +205,19 @@ def main():
     # disk_plot.setRenderHint(QtGui.QPainter.Antialiasing)
 
     # init tabs layout
-    init_tab_layout(cpu_tab, cpu_plot)
-    init_tab_layout(memory_tab, memory_plot)
-    init_tab_layout(partitions_tab, disk_plot)
-    init_tab_layout(network_tab, network_plot)
+    init_tab_layout(cpu_tab, cpu_plot, 0, task_manager)
+    init_tab_layout(memory_tab, memory_plot, 1, task_manager)
+    init_tab_layout(partitions_tab, disk_plot, 2, task_manager)
+    init_tab_layout(network_tab, network_plot, 3, task_manager)
 
     # new thread for updating the plots
+    # maybe should I start a new thread for each plot?
     thread = threading.Thread(target=update_plots,
-                              args=(cpu_usage, memory_usage, disk_plot, network_sent_usage, network_received_usage,
-                                    task_manager,
-                                    cpu_curve, memory_curve, network_sent_curve, network_received_curve, app))
+                              args=(
+                                  cpu_tab, memory_tab, partitions_tab, network_tab, cpu_usage, memory_usage, disk_plot,
+                                  network_sent_usage, network_received_usage,
+                                  task_manager,
+                                  cpu_curve, memory_curve, network_sent_curve, network_received_curve, app))
     thread.start()
 
     tabs.show()
